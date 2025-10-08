b4186a00c153d525ac6fe8f5cbae121c392de8f2
Andres Court <andres@andrescourt.com>
2025-10-08 16:29:26 -0500

    added server function

---
+++ src/lib/auth-server.ts
@@ -0,0 +1,12 @@
+import { createServerFn } from "@tanstack/react-start";
+import { authMiddleware } from "./auth-middleware";
+
+export const getUserId = createServerFn({ method: "GET" })
+    .middleware([authMiddleware])
+    .handler(async ({ context }) => {
+        return {
+            id: context.user.id,
+            name: context.user.name,
+            image: context.user.image,
+        };
+    });
