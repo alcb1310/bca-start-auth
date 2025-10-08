40785f084f786e21678271f36fcc8f2ccab7a29d
Andres Court <andres@andrescourt.com>
2025-10-08 16:10:12 -0500

    added api route

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+// import {
+//     createStartHandler,
+//     defaultStreamHandler,
+// } from "@tanstack/react-start/server";
+// import { getRouter } from "./router";
+//
+// export default createStartHandler({
+//     getRouter,
+// })(defaultStreamHandler);
+
+import {
+    createStartHandler,
+    defaultStreamHandler,
+    defineHandlerCallback,
+} from "@tanstack/react-start/server";
+
+const customHandler = defineHandlerCallback((ctx) => {
+    // add custom logic here
+    return defaultStreamHandler(ctx);
+});
+
+const fetch = createStartHandler(customHandler);
+
+export default {
+    fetch,
+};
