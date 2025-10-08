4ef98f316ef576340eef177def682361a5e95a86
Andres Court <andres@andrescourt.com>
2025-10-07 21:57:38 -0500

    home page

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+// src/routes/index.tsx
+
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+    component: Home,
+});
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+}
