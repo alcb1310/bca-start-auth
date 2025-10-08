1405d9f0dba178040f1cfcfd7aee6df8879e3094
Andres Court <andres@andrescourt.com>
2025-10-07 22:26:58 -0500

    db schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,8 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const db = drizzle(process.env.DATABASE_URL as string);
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
