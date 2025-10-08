15702337adfb07eb804faa1e20083fad46185ea6
Andres Court <andres@andrescourt.com>
2025-10-07 22:46:40 -0500

    auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { account, session, user, verification } from "../db/auth-schema";
 
 const db = drizzle(process.env.DATABASE_URL as string);
 
@@ -8,4 +9,10 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
+    schma: {
+        user,
+        session,
+        account,
+        verification,
+    },
 });
