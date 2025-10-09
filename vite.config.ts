2d98bb99c3447f237ecc1f38fbd8d57679c48dff
Andres Court <andres@andrescourt.com>
2025-10-09 13:00:15 -0500

    netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
     server: {
@@ -12,6 +13,7 @@ export default defineConfig({
         tailwindcss(),
         tsConfigPaths(),
         tanstackStart(),
+        netlify(),
         // react's vite plugin must come after start's vite plugin
         viteReact(),
     ],
