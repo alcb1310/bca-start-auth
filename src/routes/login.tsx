6b43441c41b4eb1c2ea1d8b643aab48bd18cf724
Andres Court <andres@andrescourt.com>
2025-10-10 11:22:28 -0500

    use react hook form in login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,8 @@
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { H1 } from '@/components/ui/typography'
+import { useAppForm } from '@/hooks/app.form'
 import { signIn } from '@/lib/auth-client'
 import { Link, createFileRoute, useNavigate } from '@tanstack/react-router'
-import { type ChangeEvent, type FormEvent, useState } from 'react'
 
 export const Route = createFileRoute('/login')({
     component: RouteComponent,
@@ -12,68 +10,61 @@ export const Route = createFileRoute('/login')({
 
 function RouteComponent() {
     const navigate = useNavigate()
-    const [login, setLogin] = useState<{ email: string; password: string }>({
-        email: '',
-        password: '',
+
+    const form = useAppForm({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: async ({ value }) => {
+            await signIn.email(
+                {
+                    email: value.email,
+                    password: value.password,
+                },
+                {
+                    onSuccess: () => {
+                        navigate({ to: '/dashboard' })
+                    },
+                    onError: (error) => {
+                        console.error(error)
+                        alert(error.error.message)
+                    },
+                },
+            )
+        },
     })
-
-    function handleChange(event: ChangeEvent<HTMLInputElement>) {
-        setLogin({
-            ...login,
-            [event.target.name]: event.target.value,
-        })
-    }
-
-    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-
-        const { data, error } = await signIn.email(
-            {
-                email: login.email,
-                password: login.password,
-            },
-            {
-                onSuccess: () => {
-                    console.log('Logged in')
-                    navigate({ to: '/dashboard' })
-                },
-                onError: (error) => {
-                    console.error(error)
-                    alert(error.error.message)
-                },
-            },
-        )
-
-        console.log({ data, error })
-    }
 
     return (
         <div>
             <H1>Login</H1>
-            <form onSubmit={handleSubmit}>
-                <div className='flex gap-2'>
-                    <Label htmlFor='email'>Email</Label>
-                    <Input
-                        type='email'
-                        id='email'
-                        name='email'
-                        value={login.email}
-                        onChange={handleChange}
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    form.handleSubmit()
+                }}
+            >
+                <form.AppField name='email'>
+                    {(field) => (
+                        <field.TextField label='Email' placeholder='Email' />
+                    )}
+                </form.AppField>
+                <form.AppField name='password'>
+                    {(field) => (
+                        <field.PasswordField
+                            label='Contraseña'
+                            placeholder='su contraseña'
+                        />
+                    )}
+                </form.AppField>
+
+                <form.AppForm>
+                    <form.SuscribeButton
+                        label='Login'
+                        className='uppercase tracking-wide font-bold'
                     />
-                </div>
-                <div className='flex gap-2'>
-                    <Label htmlFor='password'>Password</Label>
-                    <Input
-                        type='password'
-                        id='password'
-                        name='password'
-                        value={login.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <Button variant='default' type='submit'>
-                    Login
-                </Button>
+                </form.AppForm>
             </form>
             <Button variant='link' size='sm'>
                 <Link to='/register'>Register</Link>
