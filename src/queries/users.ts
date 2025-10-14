9baa1c225d0032b380318d50bec0855b6219018b
Andres Court <andres@andrescourt.com>
2025-10-14 17:53:35 -0500

    added logging

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -2,5 +2,6 @@ import { authClient } from '@/lib/auth-client'
 
 export async function listUsers() {
     const { data } = await authClient.admin.listUsers({ query: {} })
+    console.log('listUsers', data)
     return data ? data.users : []
 }
