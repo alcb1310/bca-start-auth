3356c14f00fb62ab795649283c812bc017c93758
Andres Court <andres@andrescourt.com>
2025-10-13 21:10:50 -0500

    display health in dashboard

--- src/routes/_authed/dashboard.tsx
+++ src/routes/_authed/dashboard.tsx
@@ -1,19 +1,27 @@
 import { Parragraph } from '@/components/ui/typography'
+import { getHealth } from '@/queries/health'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authed/dashboard')({
     component: RouteComponent,
-    beforeLoad: async ({ context }) => {
-        console.info('Token: ', context.token)
+    loader: async ({ context }) => {
+        const health = await context.queryClient.ensureQueryData({
+            queryKey: ['health'],
+            queryFn: () => getHealth({ data: { token: context.token } }),
+        })
+        return { health }
     },
 })
 
 function RouteComponent() {
     const { user } = Route.useRouteContext()
+    const { health } = Route.useLoaderData()
 
     return (
         <div>
             <Parragraph> User: {user.name}</Parragraph>
+
+            <Parragraph>Server Health {health.message}</Parragraph>
         </div>
     )
 }
