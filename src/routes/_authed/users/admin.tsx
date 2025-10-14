7f485ab73072cb6c0c89e3b9c9b60df96c30a3be
Andres Court <andres@andrescourt.com>
2025-10-14 17:12:50 -0500

    Display all users in a table

--- src/routes/_authed/users/admin.tsx
+++ src/routes/_authed/users/admin.tsx
@@ -1,7 +1,10 @@
+import { DataTable } from '@/components/ui/DataTable'
 import { Button } from '@/components/ui/button'
-import { H4, Parragraph } from '@/components/ui/typography'
+import { H4 } from '@/components/ui/typography'
 import { authClient } from '@/lib/auth-client'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import type { UserWithRole } from 'better-auth/plugins'
 import { PlusIcon, TrashIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/_authed/users/admin')({
@@ -29,7 +32,47 @@ export const Route = createFileRoute('/_authed/users/admin')({
 })
 
 function RouteComponent() {
+    const { user } = Route.useRouteContext()
     const { users } = Route.useLoaderData()
+
+    const columns: ColumnDef<UserWithRole>[] = [
+        {
+            header: 'Nombre',
+            accessorKey: 'name',
+        },
+        {
+            header: 'Email',
+            accessorKey: 'email',
+        },
+        {
+            header: 'Rol',
+            accessorKey: 'role',
+        },
+        {
+            header: 'Acciones',
+            accessorKey: 'actions',
+            enableSorting: false,
+            enableHiding: false,
+            cell: ({ row }) => (
+                <div className='flex items-center gap-2'>
+                    {user.id !== row.original.id && (
+                        <Button
+                            variant='ghost'
+                            className='text-destructive'
+                            size='sm'
+                            onClick={() =>
+                                authClient.admin.removeUser({
+                                    userId: row.original.id,
+                                })
+                            }
+                        >
+                            <TrashIcon />
+                        </Button>
+                    )}
+                </div>
+            ),
+        },
+    ]
 
     return (
         <div>
@@ -42,20 +85,7 @@ function RouteComponent() {
                 </Link>
             </Button>
 
-            {users.map((user) => (
-                <div key={user.id}>
-                    <Parragraph>
-                        {user.name} / {user.email} / {user.role} /
-                        <Button
-                            size='icon-sm'
-                            variant='ghost'
-                            className='text-destructive'
-                        >
-                            <TrashIcon />
-                        </Button>
-                    </Parragraph>
-                </div>
-            ))}
+            <DataTable columns={columns} data={users} />
         </div>
     )
 }
