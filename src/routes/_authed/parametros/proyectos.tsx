7c52b43c9ce3b54d0bc306e45dee8ea72a757e13
Andres Court <andres@andrescourt.com>
2025-10-19 12:12:56 -0500

    added a table to the projects route

--- src/routes/_authed/parametros/proyectos.tsx
+++ src/routes/_authed/parametros/proyectos.tsx
@@ -1,9 +1,105 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { DataTable } from '@/components/ui/DataTable'
+import { Button } from '@/components/ui/button'
+import { Switch } from '@/components/ui/switch'
+import { H4 } from '@/components/ui/typography'
+import {
+    getAllProyectos,
+    type proyectsResponseType,
+} from '@/queries/parametros/proyectos'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PencilIcon, PlusIcon, TrashIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/_authed/parametros/proyectos')({
-  component: RouteComponent,
+    component: RouteComponent,
+    loader: async ({ context }) => {
+        context.queryClient.invalidateQueries({
+            queryKey: ['proyectos'],
+        })
+        await context.queryClient.ensureQueryData({
+            queryKey: ['proyectos'],
+            queryFn: () => getAllProyectos({ data: { token: context.token } }),
+        })
+    },
 })
 
 function RouteComponent() {
-  return <div>Hello "/parametros/proyectos"!</div>
+    const { token } = Route.useRouteContext()
+    const { data: proyectos } = useSuspenseQuery({
+        queryKey: ['proyectos'],
+        queryFn: () => getAllProyectos({ data: { token } }),
+    })
+
+    console.log('proyectos', proyectos)
+
+    const column: ColumnDef<proyectsResponseType>[] = [
+        { header: 'Nombre', accessorKey: 'name' },
+        {
+            header: 'Area Bruta',
+            size: 400,
+            accessorKey: 'gross_area',
+            cell: ({ row }) => (
+                <span className='block text-right'>
+                    {row.original.gross_area.toLocaleString('es-EC', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                    })}
+                </span>
+            ),
+        },
+        {
+            header: 'Area Neta',
+            accessorKey: 'net_area',
+            cell: ({ row }) => (
+                <span className='block text-right'>
+                    {row.original.net_area.toLocaleString('es-EC', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                    })}
+                </span>
+            ),
+        },
+        {
+            header: 'Activo',
+            accessorKey: 'is_active',
+            cell: ({ row }) => (
+                <div className='flex items-center justify-center'>
+                    <Switch checked={row.original.is_active} disabled />
+                </div>
+            ),
+        },
+        {
+            header: 'Acciones',
+            accessorKey: 'actions',
+            cell: ({ row }) => (
+                <div className='flex items-center gap-2'>
+                    <Button
+                        variant='ghost'
+                        className='text-destructive'
+                        size='sm'
+                    >
+                        <TrashIcon />
+                    </Button>
+                    <Button variant='ghost' className='text-warning' size='sm'>
+                        <PencilIcon />
+                    </Button>
+                </div>
+            ),
+        },
+    ]
+
+    return (
+        <div>
+            <H4>Proyectos</H4>
+            <Button variant={'link'}>
+                <Link to='/dashboard' className='flex items-center gap-2'>
+                    <PlusIcon />
+                    Crear Proyecto
+                </Link>
+            </Button>
+
+            <DataTable data={proyectos} columns={column} />
+        </div>
+    )
 }
