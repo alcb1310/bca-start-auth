5dfdfa4944a60a98ae197ae2d9fbc5ac1a73af5e
Andres Court <andres@andrescourt.com>
2025-10-20 17:35:23 -0500

    change app title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,7 +27,7 @@ export const Route = createRootRouteWithContext<{
                 content: 'width=device-width, initial-scale=1',
             },
             {
-                title: 'TanStack Start Starter',
+                title: 'Sistema Control Presupuestario',
             },
         ],
         links: [
